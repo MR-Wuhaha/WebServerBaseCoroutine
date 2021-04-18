d32204e0cb824cd7195ac2c44682a23d9250b369
MR-Wuhaha <826782139@qq.com>
2021-04-18 14:29:12 +0800

    fix

--- Epoll.cpp
+++ Epoll.cpp
@@ -6,8 +6,3 @@ Epoll::Epoll():co_epoll(nullptr)
     co_epoll = co_get_epoll_ct();
     assert(co_epoll != nullptr);
 }
-
-vector<SP_channel> Epoll::Poll()
-{
-    co_eventloop(co_get_epoll_ct(),0,0);
-}
